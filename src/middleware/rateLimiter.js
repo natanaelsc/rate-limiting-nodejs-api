79093c2d7d778dba5b2989841192042a6890e26e
natanaelsc <natanael96@live.com>
2023-07-29 20:19:33 -0300

    add rate limiting status code

--- src/middleware/rateLimiter.js
+++ src/middleware/rateLimiter.js
@@ -3,7 +3,11 @@ const rateLimit = require('express-rate-limit');
 const limiter = rateLimit({
   windowMs: 60 * 1000, // 1 minute
   max: 10, // maximum number of requests allowed in the windowMs
-  message: 'Too many requests, please try again later.',
+  statusCode: 429,
+  message: {
+    message: 'Too many requests, please try again later.',
+  },
+
 });
 
 module.exports = limiter;
